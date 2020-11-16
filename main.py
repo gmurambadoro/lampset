627dbb9f5253dff5e5ee204d87fb9bdff71b3add
gmurambadoro <developer.gavin@gmail.com>
2020-11-16 23:23:23 +0200

    Add stage headings/titles to for progress indication

--- main.py
+++ main.py
@@ -3,6 +3,13 @@ from getpass import getpass
 
 from command import Command
 
+
+def heading(title: str) -> None:
+    print()
+    print(f"== {title} ==".upper())
+    print()
+
+
 try:
     # check if the platform is supported
     if sys.platform != 'linux':
@@ -11,9 +18,13 @@ try:
 
         sys.exit(1)
 
+    heading("System Upgrade")
+
     # upgrade the system first
     Command.run('sudo apt update', False)
     Command.run('sudo apt upgrade -y', False)
+
+    heading("Registering PPAs")
 
     # register required PPAs
     for ppa in ['ppa:ondrej/apache2', 'ppa:ondrej/php']:
@@ -21,8 +32,12 @@ try:
 
     Command.run('sudo apt update -y', False)
 
+    heading("Installing Apache and MySQL")
+
     Command.run('sudo apt install -y curl apache2 libapache2-mod-fcgid', False)
     Command.run('sudo apt install -y mysql-server mysql-client', False)
+
+    heading("Installing Multiple PHP Versions")
 
     # install multiple php versions
     for ver in ['5.6', '7.1', '7.2', '7.3', '7.4']:
@@ -35,11 +50,15 @@ try:
     # set the default php version
     Command.run('sudo update-alternatives --config php', False)
 
+    heading("Configuring Default PHP Version for Apache")
+
     # set default apache php to 7.4
     Command.run('sudo a2dismod php5.6', False)
     Command.run('sudo systemctl restart apache2', False)
     Command.run('sudo a2enmod php7.4', False)
     Command.run('sudo systemctl restart apache2', False)
+
+    heading("Setting Up User Permissions")
 
     # add the current user to the www-data group
     Command.run('sudo usermod -a -G www-data $USER', False)
@@ -48,23 +67,31 @@ try:
     # allow apache2 to have read/write access to virtualbox shared folders
     Command.run('sudo usermod -a -G vboxsf www-data', False)
 
+    heading("WKHTMLTOPDF")
+
     # install wkhtmltopdf
     Command.run('sudo apt install -y wkhtmltopdf', False)
 
-    # install composer
+    heading("COMPOSER")
+
     Command.run('bash composer-install.sh', False)
     Command.run('sudo chmod +x composer.phar', False)
     Command.run('sudo mv composer.phar /usr/local/bin/composer', False)
     Command.run('composer --version', False)
 
-    # configure mysql
-    Command.run('sudo mysql_secure_installation', False)
+    heading("Configuring MySQL")
+
+    res = input('Do you want to secure your MySQL installation by running `mysql_secure_installation`? (Y/n): ')
+
+    if 'y' == str(res or '').strip().lower():
+        Command.run('sudo mysql_secure_installation', False)
 
     msg = """
-If you connect to MySQL via third party clients like Workbench and phpMyAdmin you might run into
-authentication issues.
-Do you want to fix the MySQL configuration to allow for clients like Workbench and phpMyAdmin to connect? (Y/n):   
-    """
+If you frequently connect to MySQL via third party clients like Workbench and phpMyAdmin you might run into
+`MySQL Access Denied` issues.
+
+Do you want to fix the configuration file to allow for clients like Workbench and phpMyAdmin 
+to connect to MySQL? (Y/n):  """
 
     yes = str(input(msg)).strip().lower() == 'y'
 
@@ -91,6 +118,8 @@ mysql> quit
 
         Command.run('sudo mysql', True)
 
+    print()
+    print("[OK] Thank you for your patience. The setup is now complete.")
 except RuntimeError as e:
     print()
     print(e)
